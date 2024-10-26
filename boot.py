ff68fab9bbfefaab489fd867eb78add9b75d5f2d
Nrvnqsr <wolf.sama@gmail.com>
2024-10-26 16:38:06 +0200

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,14 @@
+# Download the "wifi_manager.py" file to your device;
+
+# Import the library:
+from wifi_manager import WifiManager
+
+# Initialize it
+wm = WifiManager()
+
+# By default the SSID is WiFiManager and the password is wifimanager.
+# You can customize the SSID and password of the AP for your needs:
+wm = WifiManager(ssid="RaidBox",password="RaidBox1")
+
+# Start the connection:
+wm.connect()
